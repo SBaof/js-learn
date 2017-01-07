2289346a22c6559444955b0aa275e59c9fbe0721
sbaof <184040280@qq.com>
2017-01-07 15:41:01 +0800

    updated

--- js/entry.js
+++ js/entry.js
@@ -1,4 +1,14 @@
 require ('./module-one');
 require ('./module-two');
 
+import Vue from 'vue';
+import Foo from './components/foo.vue';
+
+new Vue({
+	el: '#app',
+	components: {
+		Foo
+	}
+})
+
 require ('../style/main.css');
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
+var debug = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-    devtool: "sourcemap",
+    devtool: debug ? "sourcemap" : null,
     entry: "./js/entry.js",
     output: {
         filename: "bundle.js"
     },
-    // devServer: {
-    //     historyApiFallback: true,
-    //     hot: true,
-    //     inline: true,
-    //     progress: true,
-    // },
     module: {
         loaders: [
 	        {
@@ -20,9 +16,24 @@ module.exports = {
 	            test: /\.js$/,
 	            loader: "babel",
 	            exclude: /node_modules/
+	        },
+	        {
+	        	test: /\.vue$/,
+	        	loader: "vue"
 	        }
         ]
     },
+    resolve: {
+		alias: {
+			'vue$': 'vue/dist/vue.common.js'
+		}
+	},
+    plugins: debug ? [] : [
+    	new webpack.BannerPlugin('This file is created by Bao'),
+    	new webpack.optimize.DedupePlugin(),
+    	new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
+
+    ],
     babel: {
         presets: ['es2015', 'stage-0'],
         plugins: ['transform-runtime']
