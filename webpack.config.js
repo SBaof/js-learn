d1c947de3803d70decdb25517341942a3ca449d3
sbaof <184040280@qq.com>
2017-01-07 11:57:57 +0800

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 module.exports = {
-	devtool: "sourcemap",
-	entry: "./js/entry.js",
-	output: {
-		filename: "bundle.js"
-	},
- 	module: {
+    devtool: "sourcemap",
+    entry: "./js/entry.js",
+    output: {
+        filename: "bundle.js"
+    },
+    // devServer: {
+    //     historyApiFallback: true,
+    //     hot: true,
+    //     inline: true,
+    //     progress: true,
+    // },
+    module: {
         loaders: [
-            {
-                test: /\.css$/,
-                loader: "style!css"
-            },
-            {
-            	test: /\.js$/,
-            	loader: "babel",
-            	exclude: /node_modules/
-            }
+	        {
+	            test: /\.css$/,
+	            loader: "style!css"
+	        },
+	        {
+	            test: /\.js$/,
+	            loader: "babel",
+	            exclude: /node_modules/
+	        }
         ]
-  	},
-  	babel: {
-  		presets: ['es2015', 'stage-0'],
-  		plugins: ['transform-runtime']
-  	}
+    },
+    babel: {
+        presets: ['es2015', 'stage-0'],
+        plugins: ['transform-runtime']
+    }
 };
